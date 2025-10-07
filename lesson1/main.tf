17177f111c9b09b33b8bcb1f63751be804b489af
Oleksandr Litke <litke.programming@gmail.com>
2025-10-07 10:58:28 +0300

    #0005 [OLITKE] Add vpc configuration

--- lesson1/main.tf
+++ lesson1/main.tf
@@ -2,3 +2,18 @@ provider "aws" [
     profile = "default"
     region = "us-east-1"
 ]
+
+resource "aws_vpc" "main"{
+    cidr_block = "192.168.0.0/16"
+    tags = {
+        Nane = "mail"
+    }
+}
+
+resource "aws_subnet" "main"{
+    vpc_id = aws_vpc.mail.id
+    cidr_block = "192.168.1.0/24"
+    tags = {
+        Nane = "mail"
+    }
+}
